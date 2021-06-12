818ba6edd38f0c5d2c08b90aef2c5a1229ffa1d5
Nikolai Lvov <nikolailvov@Nikolais-MacBook-Pro.local>
2021-06-11 18:07:19 -0700

    I set the data to a state propert called this.state.plants. After that, built a custom hook called useForm using the handleChanges that was originally in CheckoutForm.

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,7 +3,12 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  constructor() {
+    super();
+    this.state = {
+      plants: [],
+    }
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
@@ -12,8 +17,13 @@ export default class PlantList extends Component {
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
       .then(res => {
-        console.log(res);
-      });
+        // console.log(res);
+        this.setState({
+          ... this.state,
+          plants: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
   }
 
   render() {
