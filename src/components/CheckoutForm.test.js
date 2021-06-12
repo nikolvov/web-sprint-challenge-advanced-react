2eff54290f5137dcc943959a03e7640d6ec7cde0
Nikolai Lvov <nikolailvov@Nikolais-MacBook-Pro.local>
2021-06-11 18:20:41 -0700

    Passed first test to make sure header renders.

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,23 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event"
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+    const header = screen.getByText(/Checkout Form/i);
+    expect(header).toBeInTheDocument();
+});
+    
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+    const { getByLabelText } = screen;
+    const first = getByLabelText(/First Name/i);
+    userEvent.type(first, "Niko");
+    const last = getByLabelText(/Last Name/i);
+    userEvent.type(last, "Lvov");
+    screen.debug();
+});
 
-test("form shows success message on submit with form details", () => {});
